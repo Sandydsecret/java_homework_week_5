5696c5dfbb5cb643623a7b09ced6611a419d93f6
Sandydsecret <126623748+Sandydsecret@users.noreply.github.com>
2023-03-07 11:41:22 +0000

    Update Programme_10.java

--- src/Homework_week05/Programme_10.java
+++ src/Homework_week05/Programme_10.java
@@ -4,7 +4,7 @@ public class Programme_10 {
     public static void main(String[] args) {
          int a=25;
          int b=5;
-         System.out.printf("%d*%d=%d%n",a,b,a*b);
-
+         System.out.print("25×5=");
+        System.out.println(a*b);
     }
 }
